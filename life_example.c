c6fffbb931fe7e2cab16042471ff354fa3ba2643
Kyle Weaver <kylecweaver@gmail.com>
2017-05-29 17:37:00 +0800

    Let user input a probability that a cell will start alive

--- life_example.c
+++ life_example.c
@@ -2,32 +2,51 @@
 #include <math.h>
 #include <time.h>
 
+void show_usage(char* program_call)
+{
+		printf("Format: %s width height [life-probability]\n", program_call);
+		printf("Example: %s 5 5 0.7\n", program_call);
+}
+
 int main(int argc, char** argv)
 {
-	if (argc != 3)
+	if (argc != 3 && argc != 4)
 	{
 		printf("Invalid arguments\n");
-		printf("Format: %s [width] [height]\n", argv[0]);
+		show_usage(argv[0]);
 		return -1;
 	}
 	
 	int width = atoi(argv[1]);
 	int height = atoi(argv[2]);
+	if (width <= 0 || height <= 0)
+	{
+		printf("Error: Invalid dimensions.\n");
+		show_usage(argv[0]);
+		return -1;
+	}
+
+	float probability = 0.5;
+	if (argc == 4)
+	{
+		probability = atof(argv[3]);
+		if (probability <= 0)
+		{
+			printf("Error: Invalid probability.\n");
+			show_usage(argv[0]);
+			return -1;
+		}
+	}
 	
 	grid* game = new_game(width, height);
-/*
-	birth(game, 0, 2);
-	birth(game, 1, 0);
-	birth(game, 1, 2);
-	birth(game, 2, 1);
-	birth(game, 2, 2);
-*/
+
 	srand(time(NULL));
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			if (rand() % 2 == 0)
+			float random = (float)rand() / RAND_MAX;
+			if (random < probability)
 			{
 				birth(game, i, j);
 			}
